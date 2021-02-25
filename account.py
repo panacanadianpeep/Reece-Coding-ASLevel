603bb8998b29d962ec4c062c6a684fcabeefbb21
Sebastian Williamson Vega <sebastian.williamson.vega@gmail.com>
2021-02-25 15:46:51 +0100

    Python Lists and Objects Commit

--- account.py
+++ account.py
@@ -34,6 +34,7 @@ class Admin(Account):
     def __init__(self, username, password):
         super().__init__(username, password)
         self.mCanChangeOtherAccountDetails = True
+    
     def toString(self):
         temp = super().toString()
         temp = temp + "\n AdminAccess: " + str(self.mCanChangeOtherAccountDetails)
@@ -43,19 +44,25 @@ class Teacher(Account):
     def __init__(self, username, password, subject):
         super().__init__(username, password)
         self.subject = subject
+        if subject == "Math" and subject == "Maths":
+            self.username = username + "Math"
+        if subject == "English":
+            self.username = username + "English"
+
     def toString(self):
         temp = super().toString()
         temp = temp + "\n Subject: " + str(self.subject)
         return temp
 
-        
+TeacherList = []
 
+for i in range(3):
+    username = input("Enter a username")
+    password = input("Enter a password")
+    subject = input("Enter a subject")
+    teacher = Teacher(username, password, subject)
+    TeacherList.append(teacher)
+    print(teacher.toString())
 
-account = Account("Reece", "Draper")
-print(account.toString())
-
-admin = Admin("Sebastian", "Williamson")
-print(admin.toString())
-
-teacher = Teacher("Juan", "Pedro", "C.S.")
-print(teacher.toString())
+for teacher in TeacherList:
+    print(teacher.toString())
